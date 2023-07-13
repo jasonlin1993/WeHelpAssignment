a0f448772cfc4cae0b63caf4ae76cc7afd2b251c
jasonlin1993 <ninjachen.lin@gmail.com>
2023-07-13 17:20:18 +0800

    Delete task files

--- week2_additional/task2.py
+++
@@ -1,9 +0,0 @@
-def two_sum(nums, target):
-    for i in range(len(nums)):
-        for j in range(i+1, len(nums)):
-            if nums[i] + nums[j] == target:
-                return [i, j]
-
-
-result = two_sum([2, 11, 7, 15], 9)
-print(result)  # show [O, 2] because nums[O]+nums[2] is 9
