8e72b9f4bf2b11a0a1971f40bac3338cdf31b018
jasonlin1993 <ninjachen.lin@gmail.com>
2023-07-18 22:59:14 +0800

    ver2

--- week3/task3/javascript/task3and4.js
+++ week3/task3/javascript/task3and4.js
@@ -1,14 +1,30 @@
 // 1. 定義 API URL
 let url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json';
+let loadedElements = 3;
+let results = [];
 
-let loadedElements = 3; // 已經載入的元素數量
-let results = []; // 儲存從 API 取得的資料
-
-// 2. 使用 fetch 函數取得 api 資料
 fetch(url)
-    .then(response => response.json())
+    .then(response => response.json()) 
     .then(data => {
         results = data.result.results;
+        
+        for(let i=0; i<3; i++){
+            let newDiv = document.createElement("div");
+            newDiv.className = "container__section__Promotion";
+            let fileString = results[i].file;
+            let firstUrl = fileString.split('https')[1];
+            firstUrl = 'https' + firstUrl;
+            let titleName = document.createElement("p");
+            let img = document.createElement('img');
+            img.src = firstUrl;
+            img.className = "container__section__Promotion__picture" ;
+            titleName.className = "container__section__Promotion__text";
+            titleName.innerText = results[i].stitle; 
+            newDiv.appendChild(img);
+            newDiv.appendChild(titleName);
+            document.querySelector('.container__section').appendChild(newDiv);
+        }
+
         loadElements();
     });
 
