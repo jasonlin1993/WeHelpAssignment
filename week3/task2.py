6f8142f0ac0e76528359903e30c0422a1694a0dc
jasonlin1993 <ninjachen.lin@gmail.com>
2023-07-20 15:45:29 +0800

    ver3

--- week3/task2.py
+++ week3/task2.py
@@ -1,70 +1,77 @@
 import bs4
 import urllib.request as req
+import threading
+import queue
+
 url = 'https://www.ptt.cc/bbs/movie/index.html'
 movie_articles = []  # 用來存放抓取到的文章
-count = 0
+article_urls = queue.Queue()  # 用來存放文章的 URL
 
-while count < 3:
-    # 建立一個 Request 物件，附加 Request Headers 的資訊
+
+def parse_html(url):
     request = req.Request(url, headers={
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
     })
     with req.urlopen(request) as response:
         data = response.read().decode('utf-8')
+    return bs4.BeautifulSoup(data, 'html.parser')
 
-    # 解析原始碼，取得每篇文章的標題中的文字
-    # 讓 BeautifulSoup 協助我們解析 HTML 格式文件
-    root = bs4.BeautifulSoup(data, 'html.parser')
 
-    # 尋找所有 class = 'title' 的 div 標籤
+def get_page_info(root):
     titles = root.find_all('div', class_='title')
-
-    # 推文數量
     likes = root.find_all('div', class_='nrec')
-
-    # 上一頁
     previous_page = root.find(
         'div', 'btn-group btn-group-paging').find_all('a')[1]['href']
 
-    # 確保 文章標題 和 推文數 有相同數量的元素
+    return titles, likes, previous_page
+
+
+def collect_articles_info(titles, likes, previous_page):
+    global article_urls, movie_articles
     min_len = min(len(titles), len(likes))
-
     for i in range(min_len):
-
-        # 確保標題存在
         if titles[i].a is not None:
             movie_title = titles[i].a.string
-
-            # 取得文章的URL
             article_url = 'https://www.ptt.cc' + titles[i].a['href']
-
-            # 建立一個新的 Request 來獲取文章的頁面內容
-            article_request = req.Request(article_url, headers={
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
-            })
-
-            with req.urlopen(article_request) as article_response:
-                article_data = article_response.read().decode('utf-8')
-                article_root = bs4.BeautifulSoup(article_data, 'html.parser')
-
-                # 文章發佈時間
-                time = article_root.find_all(
-                    'span', 'article-meta-value')[-1].string
-
+            article_urls.put((movie_title, article_url))  # 將標題與URL一起存入
             try:
                 like_count = likes[i].span.text
-
             except AttributeError:
                 like_count = '0'
+            movie_articles.append([movie_title, like_count])
 
-            # 將 文章標題 和 推文數 加到 movie_articles 列表中
-            movie_articles.append(movie_title + ',' + like_count + ',' + time)
 
-        # 更新為上一頁
-        url = "https://www.ptt.cc" + previous_page
+def add_article_time(title, url):
+    article_root = parse_html(url)
+    time = article_root.find_all('span', 'article-meta-value')[-1].string
+    return [title, time]
 
-    count += 1
+
+def worker():
+    while not article_urls.empty():
+        title, url = article_urls.get()  # 獲取標題與URL
+        time_info = add_article_time(title, url)
+        for article in movie_articles:
+            if article[0] == time_info[0]:
+                article.append(time_info[1])  # 加入時間
+                break
+
+
+for _ in range(3):
+    root = parse_html(url)
+    titles, likes, previous_page = get_page_info(root)
+    collect_articles_info(titles, likes, previous_page)
+    url = 'https://www.ptt.cc' + previous_page  # 更新 url 為上一頁的頁面
+
+threads = []
+for _ in range(10):  # 建立 10 個工作緒
+    t = threading.Thread(target=worker)
+    t.start()
+    threads.append(t)
+
+for t in threads:  # 等待所有的工作緒完成任務
+    t.join()
 
 with open("movie.txt", "w", encoding='utf-8') as f:
     for article in movie_articles:
-        f.write(article + "\n")
+        f.write(','.join(article) + "\n")
--- week3/task3/javascript/task3and4.js
+++ week3/task3/javascript/task3and4.js
@@ -3,26 +3,35 @@ let url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-
 let loadedElements = 3;
 let results = [];
 
+function createElement(result, divClass, imgClass, textClass) {
+    let newDiv = document.createElement("div");
+    newDiv.className = divClass;
+    let fileString = result.file;
+    let firstUrl = fileString.split('https')[1];
+    firstUrl = 'https' + firstUrl;
+    let titleName = document.createElement("p");
+    let img = document.createElement('img');
+    img.src = firstUrl;
+    img.className = imgClass;
+    titleName.className = textClass;
+    titleName.innerText = result.stitle;
+    newDiv.appendChild(img);
+    newDiv.appendChild(titleName);
+    document.querySelector('.container__section').appendChild(newDiv);
+}
+
 fetch(url)
     .then(response => response.json()) 
     .then(data => {
         results = data.result.results;
         
         for(let i=0; i<3; i++){
-            let newDiv = document.createElement("div");
-            newDiv.className = "container__section__Promotion";
-            let fileString = results[i].file;
-            let firstUrl = fileString.split('https')[1];
-            firstUrl = 'https' + firstUrl;
-            let titleName = document.createElement("p");
-            let img = document.createElement('img');
-            img.src = firstUrl;
-            img.className = "container__section__Promotion__picture" ;
-            titleName.className = "container__section__Promotion__text";
-            titleName.innerText = results[i].stitle; 
-            newDiv.appendChild(img);
-            newDiv.appendChild(titleName);
-            document.querySelector('.container__section').appendChild(newDiv);
+            createElement(
+                results[i],
+                'container__section__Promotion',
+                "container__section__Promotion__picture",
+                "container__section__Promotion__text"
+            )
         }
 
         loadElements();
@@ -30,24 +39,12 @@ fetch(url)
 
 function loadElements() {
     for(let i = loadedElements; i < Math.min(loadedElements + 12, results.length); i++){
-        let newDiv = document.createElement("div");
-        newDiv.className = "container__section__title";
-        let fileString = results[i].file;
-        let firstUrl = fileString.split('https')[1];
-        firstUrl = 'https' + firstUrl;
-
-        let titleName = document.createElement("p");
-        let img = document.createElement('img');
-        img.src = firstUrl;
-        img.className = "container__section__title__image" ;
-        titleName.className = "container__section__title__text";
-
-        titleName.innerText = results[i].stitle; 
-
-        newDiv.appendChild(img);
-        newDiv.appendChild(titleName);
-
-        document.querySelector('.container__section').appendChild(newDiv);
+        createElement(
+            results[i],
+            "container__section__title",
+            "container__section__title__image",
+            "container__section__title__text"
+        );
     }
 
     loadedElements += 12;
